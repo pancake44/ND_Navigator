32f3edd0a38d4d245f69bfc6f6efd91f2a5ae67c
Harrison Snow <hsnow@student04.cse.nd.edu>
2020-10-30 19:21:54 -0400

    update

--- jsfrontend/index_map.js
+++ jsfrontend/index_map.js
@@ -1,278 +1,322 @@
 console.log("load page");
 
 this.renderMap();
+getKeys();
 
 var submitButton = document.getElementById("bsr-submit-button");
 submitButton.onmouseup = getFormInfo;
 
 function getFormInfo(){
-  console.log("getting form info");
-  var names = document.getElementsByName("dest");
-  var selections = [];
-  for(let i = 0; i < names.length; i++){
-    console.log(names[i].options[names[i].selectedIndex].value);
-    selections.push(names[i].options[names[i].selectedIndex].value);
-  }
-  makeDirTable(selections);
-  addMarkers(selections);
-  makeInfoTable(selections);
+	console.log("getting form info");
+	var names = document.getElementsByName("dest");
+	var selections = [];
+	for(let i = 0; i < names.length; i++){
+		console.log(names[i].options[names[i].selectedIndex].value);
+		selections.push(names[i].options[names[i].selectedIndex].value);
+	}
+	makeDirTable(selections);
+	addMarkers(selections);
+	makeInfoTable(selections);
 }
 
 
 /* Load the google maps window */
 function loadScript(url) {
-  var index = window.document.getElementsByTagName("script")[0];
-  var script = window.document.createElement("script");
-  script.src = url;
-  script.async = true;
-  script.defer = true;
-  index.parentNode.insertBefore(script, index);
+	var index = window.document.getElementsByTagName("script")[0];
+	var script = window.document.createElement("script");
+	script.src = url;
+	script.async = true;
+	script.defer = true;
+	index.parentNode.insertBefore(script, index);
 }
 
 /* Render the Google Map */
 function renderMap() {
-  console.log("map render");
-  loadScript(
-    "https://maps.googleapis.com/maps/api/js?key=AIzaSyDwPd5fkbDSNmd5s3wWcGAKh1wOUcHwPOw&callback=initMap"
-  );
-  window.initMap = this.initMap;
+	console.log("map render");
+	loadScript(
+	"https://maps.googleapis.com/maps/api/js?key=AIzaSyDwPd5fkbDSNmd5s3wWcGAKh1wOUcHwPOw&callback=initMap"
+	);
+	window.initMap = this.initMap;
 }
 
 var myStyles = [
-  {
-    featureType: "poi",
-    elementType: "labels",
-    stylers: [{ visibility: "off" }],
-  },
+	{
+	featureType: "poi",
+	elementType: "labels",
+	stylers: [{ visibility: "off" }],
+	},
 ];
 
 /* Set map values and add markers to the map */
 var map;
 function initMap() {
-  map = new window.google.maps.Map(document.getElementById("map"), {
-    center: { lat: 41.701175, lng: -86.236569 },
-    mapTypeId: "terrain",
-    zoom: 16,
-    myStyles,
-  });
+	map = new window.google.maps.Map(document.getElementById("map"), {
+	center: { lat: 41.701175, lng: -86.236569 },
+	mapTypeId: "terrain",
+	zoom: 16,
+	myStyles,
+	});
 }
 
 function addMarkers(selections){
 
-  /* Update map with markers at locations */
-  //for(let i = 0; i < selections.length; i++){
-    new google.maps.Marker({
-      position: {lat: 41.701675, lng: -86.236769},
-      map,
-    })
+	/* Update map with markers at locations */
+	//for(let i = 0; i < selections.length; i++){
+	new google.maps.Marker({
+		position: {lat: 41.701675, lng: -86.236769},
+		map,
+	})
  // }
-  new google.maps.Marker({
-      position: {lat: 41.700898, lng: -86.236192},
-      map,
-    })
+	new google.maps.Marker({
+		position: {lat: 41.700898, lng: -86.236192},
+		map,
+	})
 }
 
 var destCount = 2;
 function addDest() {
-  console.log("adding another selection");
+	console.log("adding another selection");
 
-  /* Create a new form group div */
-  newDiv = document.createElement("div");
-  newDiv.setAttribute("class", "form-group");
-  newDiv.setAttribute("id", "origFormDiv");
+	/* Create a new form group div */
+	newDiv = document.createElement("div");
+	newDiv.setAttribute("class", "form-group");
+	newDiv.setAttribute("id", "origFormDiv");
 
 
-  /* Create a label (w/text) for new select */
-  newLab = document.createElement("label");
-  newLab.setAttribute("for", "dest" + destCount);
-  newLab.setAttribute("id", "dest" + destCount);
-  var newLabText = document.createTextNode("Destination " + destCount);
-  newLab.appendChild(newLabText);
-  newDiv.appendChild(newLab);
+	/* Create a label (w/text) for new select */
+	newLab = document.createElement("label");
+	newLab.setAttribute("for", "dest" + destCount);
+	newLab.setAttribute("id", "dest" + destCount);
+	var newLabText = document.createTextNode("Destination " + destCount);
+	newLab.appendChild(newLabText);
+	newDiv.appendChild(newLab);
 
-  /* Create a new select */
-  newSel = document.createElement("select");
-  newSel.setAttribute("id", "dest" + destCount);
-  newSel.setAttribute("name", "dest");
-  newSel.setAttribute("class", "form-control");
+	/* Create a new select */
+	newSel = document.createElement("select");
+	newSel.setAttribute("id", "dest" + destCount);
+	newSel.setAttribute("name", "dest");
+	newSel.setAttribute("class", "form-control");
 
-  /* Load options for the new select */
-  var newOptText;
-  newOpt = document.createElement("option");
-  newOpt.setAttribute("value", "41.701178");
-  newOptText = document.createTextNode("41.701178");
-  newOpt.appendChild(newOptText);
-  newSel.appendChild(newOpt);
+	/* Load options for the new select */
+	var newOptText;
+	newOpt = document.createElement("option");
+	newOpt.setAttribute("value", "41.701178");
+	newOptText = document.createTextNode("41.701178");
+	newOpt.appendChild(newOptText);
+	newSel.appendChild(newOpt);
 
-  newOpt = document.createElement("option");
-  newOpt.setAttribute("value", "-86.236574");
-  newOptText = document.createTextNode("-86.236574");
-  newOpt.appendChild(newOptText);
-  newSel.appendChild(newOpt);
+	newOpt = document.createElement("option");
+	newOpt.setAttribute("value", "-86.236574");
+	newOptText = document.createTextNode("-86.236574");
+	newOpt.appendChild(newOptText);
+	newSel.appendChild(newOpt);
 
-  /* Add the new sel to new div */
-  newDiv.appendChild(newSel);
+	/* Add the new sel to new div */
+	newDiv.appendChild(newSel);
 
-  /* Add the new div to  DOM */
-  var responseDiv = document.getElementById("here" + (destCount - 1));
-  responseDiv.appendChild(newDiv);
+	/* Add the new div to  DOM */
+	var responseDiv = document.getElementById("here" + (destCount - 1));
+	responseDiv.appendChild(newDiv);
 
-  /* Add a new p to DOM for future field adds */
-  newP = document.createElement("p");
-  newP.setAttribute("id", "here" + destCount++);
-  responseDiv.appendChild(newP);
+	/* Add a new p to DOM for future field adds */
+	newP = document.createElement("p");
+	newP.setAttribute("id", "here" + destCount++);
+	responseDiv.appendChild(newP);
 }
 
 function makeDirTable(selections){
-  console.log("entered make dir table");
+	console.log("entered make dir table");
 
-  /* Create a new div for the table */
-  newDiv = document.createElement("div");
-  newDiv.setAttribute("class", "table-responsive");
+	/* Create a new div for the table */
+	newDiv = document.createElement("div");
+	newDiv.setAttribute("class", "table-responsive");
 
-  /* Create a table */
-  newTab = document.createElement("table");
-  newTab.setAttribute("class", "table");
+	/* Create a table */
+	newTab = document.createElement("table");
+	newTab.setAttribute("class", "table");
 
-  /* Populate the header row */
-  newThead = document.createElement("thead");
-  newTrow = document.createElement("tr");
+	/* Populate the header row */
+	newThead = document.createElement("thead");
+	newTrow = document.createElement("tr");
 
-  // Order
-  var newThText;
-  newTh = document.createElement("th");
-  newTh.setAttribute("scope", "col");
-  newThText = document.createTextNode("#");
-  newTh.appendChild(newThText);
-  newTrow.appendChild(newTh);
-  // Destination
-  newTh = document.createElement("th");
-  newTh.setAttribute("scope", "col");
-  newThText = document.createTextNode("Destination");
-  newTh.appendChild(newThText);
-  newTrow.appendChild(newTh);
-  // Destination
-  newTh = document.createElement("th");
-  newTh.setAttribute("scope", "col");
-  newThText = document.createTextNode("Direction");
-  newTh.appendChild(newThText);
-  newTrow.appendChild(newTh);
+	// Order
+	var newThText;
+	newTh = document.createElement("th");
+	newTh.setAttribute("scope", "col");
+	newThText = document.createTextNode("#");
+	newTh.appendChild(newThText);
+	newTrow.appendChild(newTh);
+	// Destination
+	newTh = document.createElement("th");
+	newTh.setAttribute("scope", "col");
+	newThText = document.createTextNode("Destination");
+	newTh.appendChild(newThText);
+	newTrow.appendChild(newTh);
+	// Destination
+	newTh = document.createElement("th");
+	newTh.setAttribute("scope", "col");
+	newThText = document.createTextNode("Direction");
+	newTh.appendChild(newThText);
+	newTrow.appendChild(newTh);
 
-  /* Append header row to table */
-  newThead.appendChild(newTrow);
-  newTab.appendChild(newThead);
+	/* Append header row to table */
+	newThead.appendChild(newTrow);
+	newTab.appendChild(newThead);
 
-  newTbody = document.createElement("tbody");
-  
-  var newTbText;
-  for(let i = 0; i < selections.length; i++){
-    newTr = document.createElement("tr");
-    newTr.setAttribute("class", "accordion-toggle collapsed");
-    
-    newTd = document.createElement("td");
-    newTbText = document.createTextNode(i+1);
-    newTd.appendChild(newTbText);
-    newTr.appendChild(newTd);
+	newTbody = document.createElement("tbody");
+	
+	var newTbText;
+	for(let i = 0; i < selections.length; i++){
+	newTr = document.createElement("tr");
+	newTr.setAttribute("class", "accordion-toggle collapsed");
+	
+	newTd = document.createElement("td");
+	newTbText = document.createTextNode(i+1);
+	newTd.appendChild(newTbText);
+	newTr.appendChild(newTd);
 
-    for(let j = 0; j < 2; j++){
-      newTd = document.createElement("td");
-      newTbText = document.createTextNode(selections[i]);
-      newTd.appendChild(newTbText);
-      newTr.appendChild(newTd);
-    }
-    newTbody.appendChild(newTr);
-    newTab.appendChild(newTbody);
-    newDiv.appendChild(newTab);
-  }
-  
-  /* Create reset button */
-  newBut = document.createElement("button");
-  newBut.setAttribute("type", "button");
-  newBut.setAttribute("class", "btn btn-primary");
-  newBut.setAttribute("onclick", "showForm()");
-  var newButText = document.createTextNode("Return to form");
-  newBut.appendChild(newButText);
+	for(let j = 0; j < 2; j++){
+		newTd = document.createElement("td");
+		newTbText = document.createTextNode(selections[i]);
+		newTd.appendChild(newTbText);
+		newTr.appendChild(newTd);
+	}
+	newTbody.appendChild(newTr);
+	newTab.appendChild(newTbody);
+	newDiv.appendChild(newTab);
+	}
+	
+	/* Create reset button */
+	newBut = document.createElement("button");
+	newBut.setAttribute("type", "button");
+	newBut.setAttribute("class", "btn btn-primary");
+	newBut.setAttribute("onclick", "showForm()");
+	var newButText = document.createTextNode("Return to form");
+	newBut.appendChild(newButText);
 
-  // Hide form
-  var rhsDiv = document.getElementById("rhs");
-  rhsDiv.style.display = (
-    rhsDiv.style.display == "none" ? "block" : "none"
-  );
+	// Hide form
+	var rhsDiv = document.getElementById("rhs");
+	rhsDiv.style.display = (
+	rhsDiv.style.display == "none" ? "block" : "none"
+	);
 
-  // Show table
-  var responseDiv = document.getElementById("tab");
-  responseDiv.appendChild(newDiv);
-  responseDiv.appendChild(newBut);
+	// Show table
+	var responseDiv = document.getElementById("tab");
+	responseDiv.appendChild(newDiv);
+	responseDiv.appendChild(newBut);
 }
 
 /*
 function resetForm(){
 
-  let i = 1;
-  while(formP = document.getElementById("here" + i)){
-    formP.remove();
-    i++;
-  }
+	let i = 1;
+	while(formP = document.getElementById("here" + i)){
+	formP.remove();
+	i++;
+	}
 
-  destCount = 2;
-  var responseDiv = document.getElementById("addButton")
-  newP = document.createElement("p");
-  newP.setAttribute("id", "here1");
-  responseDiv.parentNode.insertBefore(newP, responseDiv);
+	destCount = 2;
+	var responseDiv = document.getElementById("addButton")
+	newP = document.createElement("p");
+	newP.setAttribute("id", "here1");
+	responseDiv.parentNode.insertBefore(newP, responseDiv);
 }
 */
 
 function showForm(){
 
-   
-  // Clear tables
-  var tabDiv = document.getElementById("tab");
-  tabDiv.innerHTML = "";
+	 
+	// Clear tables
+	var tabDiv = document.getElementById("tab");
+	tabDiv.innerHTML = "";
 
-  var tabInfo = document.getElementById("dtable");
-  tabInfo.style.display = "none";
+	var tabInfo = document.getElementById("dtable");
+	tabInfo.style.display = "none";
 
-  // Show the user's previous form state
-  var rhsDiv = document.getElementById("rhs");
-  rhsDiv.style.display = (
-    rhsDiv.style.display == "none" ? "block" : "none"
-  );
-  
+	// Show the user's previous form state
+	var rhsDiv = document.getElementById("rhs");
+	rhsDiv.style.display = (
+	rhsDiv.style.display == "none" ? "block" : "none"
+	);
+	
 }
 
 function makeInfoTable(selections){
-  console.log("entered make info table");
+	console.log("entered make info table");
 
-  var tabBody = document.getElementById("tbody");
-  for(let i = 0; i < selections.length; i++){
+	var tabBody = document.getElementById("tbody");
+	for(let i = 0; i < selections.length; i++){
 
-    // New row
-    newTr = document.createElement("tr");
+	// New row
+	newTr = document.createElement("tr");
 
-    // Index
-    newTd_I = document.createElement("td");
-    var newTd_I_Text = document.createTextNode(i+1);
-    newTd_I.appendChild(newTd_I_Text);
-    newTr.appendChild(newTd_I);
+	// Index
+	newTd_I = document.createElement("td");
+	var newTd_I_Text = document.createTextNode(i+1);
+	newTd_I.appendChild(newTd_I_Text);
+	newTr.appendChild(newTd_I);
 
-    // Description
-    newTd_D = document.createElement("td");
-    var newTd_D_Text = document.createTextNode(selections[i]);
-    newTd_D.appendChild(newTd_D_Text);
-    newTr.appendChild(newTd_D);
+	// Description
+	newTd_D = document.createElement("td");
+	var newTd_D_Text = document.createTextNode(selections[i]);
+	newTd_D.appendChild(newTd_D_Text);
+	newTr.appendChild(newTd_D);
 
-    // Review
-    newTd_R = document.createElement("td");
-    var newTd_R_Text = document.createTextNode(selections[i]);
-    newTd_R.appendChild(newTd_R_Text);
-    newTr.appendChild(newTd_R);
+	// Review
+	newTd_R = document.createElement("td");
+	var newTd_R_Text = document.createTextNode(selections[i]);
+	newTd_R.appendChild(newTd_R_Text);
+	newTr.appendChild(newTd_R);
 
-    // Add row to table body
-    tabBody.append(newTr);
-  }
+	// Add row to table body
+	tabBody.append(newTr);
+	}
 
-  // Make table visible
-  var tab = document.getElementById("dtable");
-  tab.style.display = "block";
+	// Make table visible
+	var tab = document.getElementById("dtable");
+	tab.style.display = "block";
 }
 
+
+function getKeys(){
+	console.log("entered getKeys")
+	
+	//var HOST_IND = document.getElementById('select-server-address').selectedIndex;
+	var HOST = "http://student04.cse.nd.edu"
+	//HOST = document.getElementById('select-server-address').options[HOST_IND].value;
+	
+	var PORT = "51040"
+	//PORT = document.getElementById('input-port-number').value;
+	
+	var URI = HOST + ":" + PORT + "/places/";
+	
+	var HTTP = "GET";
+	
+	var KEY = null;
+	
+	var reqInfo = {};
+	reqInfo.HTTP = HTTP;
+	reqInfo.URI = URI;
+	
+	console.log(reqInfo);
+	
+	networkCall(reqInfo); 
+}
+
+function networkCall(reqInfo){
+	var xhr = new XMLHttpRequest();
+	
+	xhr.open(reqInfo.HTTP, reqInfo.URI, true);
+	
+	xhr.onload = function(e) {
+	console.log(xhr.responseText);
+	document.getElementById('answer-label').innerHTML = xhr.responseText;
+	}
+	
+	xhr.onerror = function(e){
+		console.error(xhr.statusText);
+	}
+	
+	xhr.send(reqInfo.BODY);
+	
+}
