c1c7f38d0f693f84e446b5492e27e8c88a1493ab
gfernan2 <gfernan2@nd.edu>
2020-11-04 17:27:04 -0500

    server fix

--- server/server.py
+++ server/server.py
@@ -8,6 +8,10 @@ from placesController import PlaceController
 sys.path.append(str(pathlib.Path(__file__).parent.absolute()) + '/../ooapi/')
 from place_library import _place_database
 
+class optionsController:
+	def OPTIONS(self, *args, **kwargs):
+		return ""
+
 def CORS():
     cherrypy.response.headers["Access-Control-Allow-Origin"] = "*"
     cherrypy.response.headers["Access-Control-Allow-Methods"] = "GET, PUT, POST, DELETE, OPTIONS"
@@ -25,13 +29,17 @@ def start_service():
 	dispatcher.connect('place_index_get', '/places/', controller=placeController, action = 'GET_INDEX', conditions=dict(method=['GET']))
 	dispatcher.connect('place_index_post', '/places/', controller=placeController, action = 'POST_INDEX', conditions=dict(method=['POST']))
 
+	dispatcher.connect('place_options', '/places/', controller=optionsController, action = 'OPTIONS', conditions=dict(method=['OPTIONS']))
+	dispatcher.connect('place_key_options', '/places/:place_id', controller=optionsController, action = 'OPTIONS', conditions=dict(method=['OPTIONS']))
+
+
 	conf = {
 	'global': {
 		'server.thread_pool': 5, # optional argument
-		'server.socket_host': 'localhost', # 
+		'server.socket_host': 'student04.cse.nd.edu', # 
 		'server.socket_port': 51040, #change port number to your assigned
 			},
-	'/': {
+	'/':{
 		'request.dispatch': dispatcher,
 		'tools.CORS.on':True,
 		}
