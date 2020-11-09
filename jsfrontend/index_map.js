34c7425ae22fe84cca2ed7e2c3fceb37a18ac1b6
Harry Snow <harrysnow@hs-macbook-pro.dhcp.nd.edu>
2020-11-09 12:15:56 -0500

    support for description and reviews

--- jsfrontend/index_map.js
+++ jsfrontend/index_map.js
@@ -1,25 +1,18 @@
 console.log("load page");
 
+var map;
 this.renderMap();
 var destCount = 1;
 var markerArr = [];
+var selections = [];
 addDest();
+var currentItem;
 
 var submitButton = document.getElementById("bsr-submit-button");
 submitButton.onmouseup = getFormInfo;
 
-function getFormInfo(){
-	console.log("getting form info");
-	var names = document.getElementsByName("dest");
-	var selections = [];
-	for(let i = 0; i < names.length; i++){
-		console.log(names[i].options[names[i].selectedIndex].value);
-		selections.push(names[i].options[names[i].selectedIndex].value);
-	}
-	
-	makeDirTable(selections);
-	// makeInfoTable(selections);
-}
+
+/*** MAP FUNCTIONS ***/
 
 /* Load the google maps window */
 function loadScript(url) {
@@ -40,25 +33,44 @@ function renderMap() {
 	window.initMap = this.initMap;
 }
 
-var myStyles = [
-	{
-	featureType: "poi",
-	elementType: "labels",
-	stylers: [{ visibility: "off" }],
-	},
-];
-
 /* Set map values and add markers to the map */
-var map;
 function initMap() {
 	map = new window.google.maps.Map(document.getElementById("map"), {
 	center: { lat: 41.701175, lng: -86.236569 },
 	mapTypeId: "terrain",
-	zoom: 16,
-	myStyles,
+	zoom: 16
 	});
 }
 
+/* Add markers in array to map */
+function setMapAll(){
+	initMap();
+	for(let i = 0; i < markerArr.length; i++)
+		markerArr[i].setMap(map);
+	markerArr = [];
+}
+
+
+/*** FORM AND DIRECTION TABLE FUNCTIONS ***/
+
+/* On submission of form, put the selections into a global array and
+   construct two tables */
+function getFormInfo(){
+	console.log("getting form info");
+
+	selections = []; // Clear array from previous form sumbissions
+	var names = document.getElementsByName("dest");
+	for(let i = 0; i < names.length; i++){
+		console.log(names[i].options[names[i].selectedIndex].value);
+		selections.push(names[i].options[names[i].selectedIndex].value);
+		console.log("selection: " + selections[i]);
+	}
+	
+	makeDirTable();
+	makeInfoTable();
+}
+
+/* Add a nother dropdown selection to the form */
 async function addDest(){
 	console.log("adding another selection");
 
@@ -100,35 +112,35 @@ async function addDest(){
 		newSel.appendChild(newOpt);
 	}
 
-	/* Add the new sel to new div */
+	// Add the new sel to new div 
 	newDiv.appendChild(newSel);
 
-	/* Add the new div to DOM */
+	// Add the new div to DOM 
 	console.log("responseDiv: " + "here" + destCount);
 	var responseDiv = document.getElementById("here" + destCount);
 	responseDiv.appendChild(newDiv);
 
-	/* Add a new p to DOM for future field adds */
+	// Add a new p to DOM for future field adds 
 	newP = document.createElement("p");
 	newP.setAttribute("id", "here" + ++destCount);
 	responseDiv.appendChild(newP);
 }
 
-async function makeDirTable(selections){
+/* Creates the table with directions between locations */
+async function makeDirTable(){
 	console.log("entered make dir table");
 
-	/* Create a new div for the table */
+	// Create a new div for the table 
 	newDiv = document.createElement("div");
 	newDiv.setAttribute("class", "table-responsive");
 
-	/* Create a table */
+	// Create a table 
 	newTab = document.createElement("table");
 	newTab.setAttribute("class", "table");
 
-	/* Populate the header row */
+	// Populate the header row 
 	newThead = document.createElement("thead");
 	newTrow = document.createElement("tr");
-
 	// Order
 	var newThText;
 	newTh = document.createElement("th");
@@ -153,32 +165,34 @@ async function makeDirTable(selections){
 	newThead.appendChild(newTrow);
 	newTab.appendChild(newThead);
 
+	// Create body and populate with data from server
 	newTbody = document.createElement("tbody");
-	
+
 	var newTbText;
 	var oldxcord;
 	var oldycord;
 	var newxcord;
 	var newycord;
 	for(let i = 0; i < selections.length; i++){
+
+		// Make one server request per key
 		dataJSON = await getKeys("GET", selections[i]);
 		data = JSON.parse(dataJSON);
 		
+		// New row
 		newTr = document.createElement("tr");
-		newTr.setAttribute("class", "accordion-toggle collapsed");
-		
+		newTr.setAttribute("class", "accordion-toggle collapsed");	
 		// Order
 		newTd = document.createElement("td");
 		newTbText = document.createTextNode(i+1);
 		newTd.appendChild(newTbText);
 		newTr.appendChild(newTd);
-
 		// Destination
 		newTd = document.createElement("td");
 		newTbText = document.createTextNode(data["name"]);
 		newTd.appendChild(newTbText);
 		newTr.appendChild(newTd);
-
+		// Calculate direction
 		if(i == 0){
 			newTbody.appendChild(newTr);
 			newTab.appendChild(newTbody);
@@ -188,7 +202,7 @@ async function makeDirTable(selections){
 			oldycord = data["ycord"];
 		}
 		else{
-			// calculate distance and direction from xcord and ycord, then put in table
+			// Calculate distance and direction from xcord and ycord, then put in table
 			newxcord = data["xcord"];
 			newycord = data["ycord"];
 			
@@ -197,7 +211,7 @@ async function makeDirTable(selections){
 			console.log("Bearing: " + getBearing(oldxcord, oldycord, newxcord, newycord));
 			
 			newTd = document.createElement("td");
-			// put distance and direction into table entry
+			// Put distance and direction into table entry
 			newTbText = document.createTextNode(getDistance(oldxcord, oldycord, newxcord, newycord).toFixed(0) + " m   " + getCardinal(getBearing(oldxcord, oldycord, newxcord, newycord)));
 			newTd.appendChild(newTbText);
 			newTr.appendChild(newTd);
@@ -207,19 +221,19 @@ async function makeDirTable(selections){
 			newTab.appendChild(newTbody);
 			newDiv.appendChild(newTab);
 
-			// preserve coordinates for next point
+			// Preserve coordinates for next point
 			oldxcord = data["xcord"];
 			oldycord = data["ycord"];
 		}
 
+		// Make a marker object and add to array
 		var mark = new google.maps.Marker({
 			position: {lat: parseFloat(data["ycord"]), lng: parseFloat(data["xcord"])},
-			map: map,
 		});
 		markerArr.push(mark);
 	}
 
-	/* Create reset button */
+	// Create back button 
 	newBut = document.createElement("button");
 	newBut.setAttribute("type", "button");
 	newBut.setAttribute("class", "btn btn-primary");
@@ -230,95 +244,288 @@ async function makeDirTable(selections){
 	// Hide form
 	var rhsDiv = document.getElementById("rhs");
 	rhsDiv.style.display = (
-	rhsDiv.style.display == "none" ? "block" : "none"
+		rhsDiv.style.display == "none" ? "block" : "none"
 	);
 
 	// Show table
 	var responseDiv = document.getElementById("tab");
 	responseDiv.appendChild(newDiv);
 	responseDiv.appendChild(newBut);
+
+	// Add markers to map
+	setMapAll();
 }
 
-/*
-function resetForm(){
-
-	let i = 1;
-	while(formP = document.getElementById("here" + i)){
-	formP.remove();
-	i++;
-	}
-
-	destCount = 2;
-	var responseDiv = document.getElementById("addButton")
-	newP = document.createElement("p");
-	newP.setAttribute("id", "here1");
-	responseDiv.parentNode.insertBefore(newP, responseDiv);
-}
-*/
-
+/* Hide the direction table and pull up the previous form */
 function showForm(){
 	 
-	// Clear tables
+	// Clear previous direction table
 	var tabDiv = document.getElementById("tab");
 	tabDiv.innerHTML = "";
-
-	var tabInfo = document.getElementById("dtable");
-	tabInfo.style.display = "none";
 
 	// Show the user's previous form state
 	var rhsDiv = document.getElementById("rhs");
 	rhsDiv.style.display = (
-	rhsDiv.style.display == "none" ? "block" : "none"
+		rhsDiv.style.display == "none" ? "block" : "none"
 	);
 }
 
-function makeInfoTable(selections){
-	console.log("entered make info table");
 
+/*** INFO TABLE FUNCTIONS ***/
+
+// Build a table to show place description and reviews
+async function makeInfoTable(){
+	console.log("entered make info table");
+	
+	currentItem = 0;
+	
+	// Clear previous table
 	var tabBody = document.getElementById("tbody");
+	tabBody.innerHTML = "";
+
+	// Add a row entry for each destination
 	for(let i = 0; i < selections.length; i++){
 
-	// New row
-	newTr = document.createElement("tr");
+		dataJSON = await getKeys("GET", selections[i]);
+		data = JSON.parse(dataJSON);
 
-	// Index
-	newTd_I = document.createElement("td");
-	var newTd_I_Text = document.createTextNode(i+1);
-	newTd_I.appendChild(newTd_I_Text);
-	newTr.appendChild(newTd_I);
+		// New row
+		newTr = document.createElement("tr");
+		newTr.setAttribute("data-toggle","modal");
+		newTr.setAttribute("data-target","#mod");
+		newTr.setAttribute("data-id", i);
 
-	// Description
-	newTd_D = document.createElement("td");
-	var newTd_D_Text = document.createTextNode(selections[i]);
-	newTd_D.appendChild(newTd_D_Text);
-	newTr.appendChild(newTd_D);
+		// Index
+		newTd_I = document.createElement("td");
+		newTd_I.setAttribute("name","dtd");
+		var newTd_I_Text = document.createTextNode(i+1);
+		newTd_I.appendChild(newTd_I_Text);
+		newTr.appendChild(newTd_I);
+		
+		// Location
+		newTd_L = document.createElement("td");
+		newTd_L.setAttribute("name","dtd");
+		var newTd_L_Text = document.createTextNode(data["name"]);
+		newTd_L.appendChild(newTd_L_Text);
+		newTr.appendChild(newTd_L);
+	
+		// Description
+		newTd_D = document.createElement("td");
+		newTd_D.setAttribute("name","dtd");
+		var newTd_D_Text = document.createTextNode(data["description"]);	
+		newTd_D.appendChild(newTd_D_Text);
+		newTr.appendChild(newTd_D);
 
-	// Review
-	newTd_R = document.createElement("td");
-	var newTd_R_Text = document.createTextNode(selections[i]);
-	newTd_R.appendChild(newTd_R_Text);
-	newTr.appendChild(newTd_R);
+		// Review
+		newTd_R = document.createElement("td");
+		newTd_R.setAttribute("name","dtd");
+		var newTd_R_Text;
+		if(data["reviews"][0])
+			for(let j = 0; j < data["reviews"].length; j++)
+				newTd_R_Text = document.createTextNode(data["reviews"][j]);
+		else
+			newTd_R_Text = document.createTextNode("No reviews yet");
+		newTd_R.appendChild(newTd_R_Text);
+		newTr.appendChild(newTd_R);
 
-	// Add row to table body
-	tabBody.append(newTr);
+		// Add row to table body
+		tabBody.append(newTr);
 	}
 
 	// Make table visible
 	var tab = document.getElementById("dtable");
 	tab.style.display = "block";
+
+	// Make table rows clickable
+	addRowHandlers();
+}
+
+// Makes table rows clickable	
+function addRowHandlers(){
+	console.log("entered addRowHandlers");
+
+	var table = document.getElementById("tabled");
+	var rows = table.getElementsByTagName("tr");
+	var i;
+	for (i = 0; i < rows.length; i++) {
+		var currentRow = table.rows[i];
+    	var createClickHandler = function(row) {
+      		return function() {
+        		var cell = row.getElementsByTagName("td")[0];
+        		var id = cell.innerHTML;
+				currentItem = row.rowIndex - 1;
+				review();
+      		};
+    	};
+    	currentRow.onclick = createClickHandler(currentRow);
+  	}
+}
+
+// Pulls up modal to view/add reviews
+function review(){
+	console.log("entered review");
+
+	document.getElementById("myModal").style.display= "block";
+
+	var modal = document.getElementById("myModal");
+	var btn = document.getElementById("myBtn");
+	var span = document.getElementsByClassName("close")[0];
+
+	// Clear and close modal
+	span.onclick = function() {
+		clearReview();
+ 		modal.style.display = "none";
+	}
+
+	// Clear and close modal
+	window.onclick = function(event) {
+		if (event.target == modal) {
+			clearReview();
+    		modal.style.display = "none";
+  		}
+	}
+
+	popReview();
+}
+
+// Add previous reviews to the modal
+async function popReview(){
+	console.log("entered popReview");
+	
+	dataJSON = await getKeys("GET", selections[currentItem]);
+	data = JSON.parse(dataJSON);
+
+	// If no reviews, display and exit
+	if(!data["reviews"] || typeof data["reviews"][0] == "undefined"){
+		responseDiv = document.getElementById("reviewtag1");
+		responseDiv.innerHTML = "No reviews yet";
+		return;
+	}
+
+	// Add first review to prexisting tag
+	responseDiv = document.getElementById("reviewtag1");
+	responseDiv.innerHTML = data["reviews"][0];
+
+	// Generate new tags and add subsequent reviews
+	for(let j = 1; j < data["reviews"].length; j++){
+		
+		newDivF = document.createElement("div");
+		newDivF.setAttribute("class", "form-group");
+
+		newDivC = document.createElement("div");
+		newDivC.setAttribute("class", "col-md-11");
+
+		newTA = document.createElement("textarea");
+		newTA.setAttribute("id", "reviewtag" + (j+2));
+		newTA.setAttribute("class", "form-control");
+		newTA.setAttribute("readonly", true);
+		newTA.innerHTML = data["reviews"][j];
+		
+		newDivC.appendChild(newTA);
+		newDivF.appendChild(newDivC);
+	
+		formDiv = document.getElementById("wrapper");
+		formDiv.appendChild(newDivF);
+	}
+}
+
+// Submit a new review
+async function addReview(){
+	console.log("entered addReview");
+	console.log("input: ", document.getElementById("textarea").value);
+	console.log("selection at currentItem: " + selections[currentItem]);
+
+	dataJSON = await getKeys("GET", selections[currentItem]);
+	data = JSON.parse(dataJSON);
+
+	// Pull the place object, and change the review field
+	mbody = {"name": data["name"], "xcord": data["xcord"], "ycord": data["ycord"], "adds": data["adds"], "reviews":  document.getElementById("textarea").value, "description": data["description"]};
+
+	// PUT to the server
+	await putKeys("PUT", selections[currentItem], mbody);
+}
+
+// Clear previous reviews from the modal
+function clearReview(){
+	console.log("entered clearreview");
+	
+	if(document.getElementById("wrapper")){
+		console.log("clearing review");
+
+		tabDiv = document.getElementById("wrapper");
+		tabDiv.innerHTML = "";
+	
+		fg = document.createElement("div");
+		fg.setAttribute("class", "form-group");
+
+		cmd = document.createElement("div");
+		cmd.setAttribute("class", "col-md-11");
+		cmd.setAttribute("id", "reviewtab");
+
+		reviewTag = document.createElement("textarea");
+		reviewTag.setAttribute("class", "form-control");
+		reviewTag.setAttribute("id", "reviewtag1");
+		reviewTag.setAttribute("readonly", true);
+
+		cmd.appendChild(reviewTag);
+		fg.appendChild(cmd);
+		tabDiv.appendChild(fg);
+	}	
 }
 
 
-function getKeys(REST, KEY/*, callback*/){
+/*** NETWORK FUNCTIONS ***/
+
+function putKeys(REST, KEY, mBODY){
+	console.log("entered putKeys");
+
 	return new Promise(function (resolve, reject) {
-		console.log("entered getKeys")
+		console.log("entered putKeys");
+		console.log("REST: " + REST);
+
+		var HOST = "http://student04.cse.nd.edu";
+		var PORT = "51040";
+
+		var URI = HOST + ":" + PORT + "/places/" + KEY;
+		var HTTP = REST;
+		var BODY = JSON.stringify(mBODY);
+
+		var reqInfo = {};
+		reqInfo.HTTP = HTTP;
+		reqInfo.URI = URI;
+		reqInfo.BODY = BODY;
+
+		var xhr = new XMLHttpRequest();
+
+		xhr.open(reqInfo.HTTP, reqInfo.URI, true);
+
+		xhr.onload = function(e) {
+			console.log("responseTest" + xhr.responseText);
+			resolve(xhr.responseText);
+		}
+
+		xhr.onerror = function(e){
+			console.error(xhr.statusText);
+			reject({
+                status: this.status,
+                statusText: xhr.statusText
+            });
+		}
+
+		xhr.send(reqInfo.BODY);
+	});
+}
+
+function getKeys(REST, KEY){
+	return new Promise(function (resolve, reject) {
+		console.log("entered getKeys");
 		console.log("REST: " + REST);
 		
-		var HOST = "http://student04.cse.nd.edu"	
-		var PORT = "51040"
+		var HOST = "http://student04.cse.nd.edu";	
+		var PORT = "51040";
 		
 		var URI = HOST + ":" + PORT + "/places/";
-		var HTTP = REST
+		var HTTP = REST;
 		if(KEY)
 			URI += KEY;
 		
@@ -334,7 +541,6 @@ function getKeys(REST, KEY/*, callback*/){
 
 		xhr.onload = function(e) {
 			console.log("responseText" + xhr.responseText);
-			//callback(xhr.responseText);
 			resolve(xhr.responseText);
 		}
 		
@@ -351,25 +557,7 @@ function getKeys(REST, KEY/*, callback*/){
 }
 
 
-/*
-function networkCall(reqInfo){
-	console.log("entered networkCall");
-	var xhr = new XMLHttpRequest();
-	
-	xhr.open(reqInfo.HTTP, reqInfo.URI, true);
-	
-	xhr.onload = function(e) {
-		console.log("responseText" + xhr.responseText);
-		return xhr.responseText;
-	}
-	
-	xhr.onerror = function(e){
-		console.error(xhr.statusText);
-	}
-	
-	xhr.send(reqInfo.BODY);
-}
-*/
+/*** DIRECTION CALCULATION FUNCTIONS ***/
 
 // simple math functions for distance and direction
 // ripped from http://www.movable-type.co.uk/scripts/latlong.html
@@ -410,5 +598,4 @@ function getCardinal(bearing){
 		: (offsetAngle >= 6 * degreePerDirection && offsetAngle < 7 * degreePerDirection) ? "W"
 		: "NW";
 }
-
 
